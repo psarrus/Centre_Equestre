dda5d52899a03151676fa59ca7ffcd5f06dc53d0
Pierre Sarrus <psarrus@wanadoo.fr>
2016-04-13 12:59:35 +0200

    initial version

---
+++ eplefpa/urls.py
@@ -0,0 +1,9 @@
+# from django.conf.urls import url, include
+# from views import profil, cheval, encadrement, homepage
+#
+# urlpatterns = [
+#     url(r'^$', homepage),
+#     url(r'^profil$', profil),
+#     url(r'^cheval$', cheval),
+#     url(r'^encadrement$', encadrement),
+# ]
