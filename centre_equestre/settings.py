bb697852750e5f0110140960ad1c0fe676640212
Merkur39 <merkkur39@gmail.com>
2016-05-03 09:44:51 +0200

    modif settings

--- centre_equestre/settings.py
+++ centre_equestre/settings.py
@@ -57,10 +57,7 @@ ROOT_URLCONF = 'centre_equestre.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            # os.path.join(BASE_DIR,'centre_equestre','templates')
-            "/home/psarrus/Code/Centre_Equestre/centre_equestre/centre_equestre/templates"
-        ],
+        'DIRS': [os.path.join(BASE_DIR, 'centre_equestre', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
