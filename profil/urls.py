98c4714b995a78d6ee50341074c6162f694002a7
Merkur39 <merkkur39@gmail.com>
2016-05-11 13:42:56 +0200

    add profil & public

--- profil/urls.py
+++ profil/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import url
-from views import create_profil, edit_profil
+from views import CreateProfil, ListProfil, ProfilUpdate, CreatePublic, ListPublic, PublicUpdate
 
 
 urlpatterns = [
-    url(r'^$', create_profil),
-    url(r'^vue_profil/(?P<id>[\w-]+)$', edit_profil),
+    url(r'^$', ListProfil.as_view(), name='list_profil'),
+    url(r'^edit$', CreateProfil.as_view(), name='create_profil'),
+    url(r'^(?P<pk>[\w-]+)$', ProfilUpdate.as_view(), name='profil_update'),
+    url(r'^public/edit$', CreatePublic.as_view(), name='create_public'),
+    url(r'^public/list$', ListPublic.as_view(), name='list_public'),
+    url(r'^public/(?P<pk>[\w-]+)$', PublicUpdate.as_view(), name='public_update'),
+    # url(r'^vue_profil/(?P<id>[\w-]+)$', edit_profil),
 ]
