205e0fe7801048c5c49b515220a05fbfd2562682
merkur39 <merkkur39@gmail.com>
2016-05-18 23:38:30 +0200

    modif profil

--- monte/admin.py
+++ monte/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from models import CreneauMontoir
-from profil.models import Profil
 from cheval.models import Cheval
 
 # Register your models here.
 admin.site.register(CreneauMontoir)
-admin.site.register(Profil)
 admin.site.register(Cheval)
--- profil/admin.py
+++ profil/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from profil.models import Profil, Periode
 
-# Register your models here.
+
+class PeriodeInlineAdmin(admin.TabularInline):
+    model = Periode
+    extra = 1
+
+
+class ProfilAdmin(admin.ModelAdmin):
+    list_display  = ['nom', 'prenom', 'cp', 'ville', 'adresse', 'email', 'tel_1']
+    inlines = [PeriodeInlineAdmin]
+
+
+
+admin.site.register(Profil, ProfilAdmin)
