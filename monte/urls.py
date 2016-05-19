dbd6379a88fdb5d9098f38c131bf0f5f1221a441
jladine <ladine.johan@gmail.com>
2016-05-19 14:29:49 +0200

    creation modification et suppression des montoirs

--- monte/urls.py
+++ monte/urls.py
@@ -4,6 +4,11 @@ from views import *
 urlpatterns = [
     url(r'^$', homepage),
     url(r'^create/montoir$', CreneauMontoirCreate.as_view(), name='creneau_montoir_create'),
+    url(r'^update/montoir/(?P<pk>[\w-]+)$', CreneauMontoirUpdate.as_view(), name='creneau_montoir_update'),
+    url(r'^delete/montoir/(?P<pk>[\w-]+)$', CreneauMontoirDelete.as_view(), name='creneau_montoir_delete'),
+    url(r'^list/montoir$', CreneauMontoirList.as_view(), name='creneau_montoir_list'),
+    url(r'^piquet/montoir/(?P<pk>[\w-]+)$', PiquetMontoirStaffDetail.as_view(), name='piquet_montoir'),
+    # url(r'^piquet/montoir/(?P<id>[\w-]+)$', creneau_montoir, name='piquet_montoir'),
 
     # url(r'^create/piquet/staff$', PiquetMontoirStaff.as_view(), name='piquet_enseignant_create'),
     #
--- monte/views.py
+++ monte/views.py
@@ -1,14 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import ListView, CreateView, UpdateView
+from django.views.generic import ListView, CreateView, UpdateView, DetailView, DeleteView
 from django.core.urlresolvers import reverse_lazy
 
 from models import *
+from cheval.models import Cheval
 
 
-# def encadrement(request):
-#     return render(request,'encadrement.html')
-#
-#
 def homepage(request):
     return render(request,'homepage.html')
 
@@ -16,26 +13,49 @@ def homepage(request):
 
 class CreneauMontoirCreate(CreateView):
     model = CreneauMontoir
-    fields = ['jour','heure_debut','duree','classe','effectif','encadrant','remarque']
+    fields = ['jour','heure_debut','duree','public','effectif','encadrant','remarque']
     template_name = 'creneau_montoir_create.html'
-    success_url = reverse_lazy('homepage')
+    success_url = reverse_lazy('creneau_montoir_list')
+
+class CreneauMontoirList(ListView):
+    model = CreneauMontoir
+    template_name = 'creneau_montoir_liste.html'
+    context_object_name = 'creneaux'
+    queryset = CreneauMontoir.objects.all()
+
+class CreneauMontoirUpdate(UpdateView):
+    model = CreneauMontoir
+    fields = "__all__"
+    template_name = 'creneau_montoir_update.html'
+    success_url = reverse_lazy('creneau_montoir_list')
+
+class CreneauMontoirDelete(DeleteView):
+    model = CreneauMontoir
+    template_name = 'creneau_montoir_delete.html'
+    success_url = reverse_lazy('creneau_montoir_list')
 
 
-# class CreneauMontoirUpdate(UpdateView):
-#     model = CreneauMontoir
-#     fields = ['jour','heure_debut','duree','classe','effectif','encadrant','remarque']
-#     template_name = 'creneau_montoir_update.html'
-#     success_url = reverse_lazy('homepage')
-#
-#
-#
-#
+
 # class PiquetMontoirStaffCreate(CreateView):
 #     model = PiquetMontoirStaff
 #     fields = ['montoir', 'cheval']
 #     template_name = 'piquet_montoir_staff_create.html'
 #     success_url = reverse_lazy('homepage')
-#
+
+class PiquetMontoirStaffDetail(DetailView):
+    model = CreneauMontoir
+    template_name = 'piquet_montoir_staff_liste.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(PiquetMontoirStaffDetail, self).get_context_data(**kwargs)
+        context['creneau'] = self.get_object()
+        context['chevaux'] = Cheval.objects.all()
+
+        return context
+
+
+
+
 # class PiquetMontoirStaffUpdate(UpdateView):
 #     model = PiquetMontoirStaff
 #     fields = ['montoir', 'cheval']
