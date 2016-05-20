7a4939dac37205b882b70434264da55bdd47158f
merkur39 <merkkur39@gmail.com>
2016-05-20 10:14:02 +0200

    add display profil

--- profil/urls.py
+++ profil/urls.py
@@ -4,7 +4,7 @@ from views import CreateProfil, ListProfil, ProfilUpdate, CreatePublic, ListPubl
 
 urlpatterns = [
     url(r'^$', ListProfil.as_view(), name='list_profil'),
-    url(r'^edit$', CreateProfil.as_view(), name='create_profil'),
+    url(r'^create$', CreateProfil.as_view(), name='create_profil'),
     url(r'^(?P<pk>[\w-]+)$', ProfilUpdate.as_view(), name='profil_update'),
     url(r'^public/edit$', CreatePublic.as_view(), name='create_public'),
     url(r'^public/list$', ListPublic.as_view(), name='list_public'),
