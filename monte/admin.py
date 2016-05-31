cd9e567d1af51dc86cee8a39b3fbc5f8030c6b3b
jladine <ladine.johan@gmail.com>
2016-05-31 14:42:36 +0200

    ajout bootstrap-switch et creation de la liste des chevaux avec les filtre et signaux

--- monte/admin.py
+++ monte/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from models import CreneauMontoir
+from models import CreneauMontoir, PiquetMontoirStaff
 from cheval.models import Cheval
 
 # Register your models here.
 admin.site.register(CreneauMontoir)
+admin.site.register(PiquetMontoirStaff)
+
 admin.site.register(Cheval)
