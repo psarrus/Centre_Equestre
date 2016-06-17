1965fafe0fa04d4a6e675d86bf9f2085517aca98
Merkur39 <merkkur39@gmail.com>
2016-06-17 15:07:25 +0200

    modif settings

--- centre_equestre/settings.py
+++ centre_equestre/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
-from site_settings import DATABASES
+import * from site_settings
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -24,9 +24,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'jv!#3m=@f=++w))x_=v2%n7f_db+p!4y$7(8dgg1d=q0_rg#8o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -94,6 +91,16 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+DATABASES = {
+    'default': {
+        'NAME': 'centre_equestre',
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'merkur',
+        'PASSWORD': 'plop48000',
+        'HOST':'localhost'
+    }
+}
 
 
 # Internationalization
